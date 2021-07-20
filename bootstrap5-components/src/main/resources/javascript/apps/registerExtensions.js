8e0255b56c02340497272b7786c5e9450614ad07
pvollenweider <pvollenweider@jahia.com>
2021-07-20 16:20:40 +0200

    Use custom picker for internal link from the button, that allow to pick only jmix:droppableContent jnt:page or jnt:file

--- bootstrap5-components/src/main/resources/javascript/apps/registerExtensions.js
+++ bootstrap5-components/src/main/resources/javascript/apps/registerExtensions.js
@@ -1,8 +1,23 @@
 window.jahia.i18n.loadNamespaces('bootstrap5-components');
 
-window.jahia.uiExtender.registry.add('callback', 'bootstrap5-componentsExample', {
+window.jahia.uiExtender.registry.add('callback', 'bootstrap5-components', {
     targets: ['jahiaApp-init:60'],
     callback: function () {
+        window.jahia.uiExtender.registry.add('pickerConfiguration', 'pagesandfiles', {
+            cmp: {
+                picker: window.jahia.uiExtender.registry.get('selectorType', 'ContentPicker'),
+                treeConfigs: [{
+                    rootPath: site => `/sites/${site}`,
+                    openableTypes: ['jnt:page', 'jnt:navMenuText', 'jnt:virtualsite', 'jnt:contentFolder', 'nt:folder', 'jmix:siteContent', 'jmix:browsableInEditorialPicker'],
+                    selectableTypes: ['jnt:page', 'jnt:navMenuText', 'jnt:virtualsite', 'jnt:contentFolder', 'nt:folder', 'jmix:siteContent', 'jmix:browsableInEditorialPicker'],
+                    type: 'pagesandfiles',
+                }],
+                searchSelectorType: 'bootstrap5mix:internalLink',
+                listTypesTable: ['jmix:droppableContent', 'jnt:page', 'jnt:file'],
+                selectableTypesTable: ['jmix:droppableContent', 'jnt:page', 'jnt:file']
+            }
+        });
+        /*
         window.jahia.uiExtender.registry.add('adminRoute', 'bootstrap5-componentsExample', {
             targets: ['administration-sites:999', 'bootstrap5-componentsaccordion'],
             label: 'bootstrap5-components:label.settings.title',
@@ -30,5 +45,6 @@ window.jahia.uiExtender.registry.add('callback', 'bootstrap5-componentsExample',
                 return siteKey !== 'systemsite'
             }
         });
+        */
     }
 });
